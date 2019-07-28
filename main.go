65b79283f5c0e881240671084996691fcda220a6
David Braley <david.braley@gmail.com>
2019-07-28 00:10:45 -0400

    Insert records

--- main.go
+++ main.go
@@ -1,8 +1,79 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "gophercises_phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", host, port, user)
+	//db, err := sql.Open("postgres", psqlInfo)
+	//if err != nil {
+	//	fmt.Println("There was an error opening the database connection:", err)
+	//	panic(err)
+	//}
+	//must(resetDB(db, dbname))
+	//must(db.Close())
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(db.Ping())
+	must(createPhoneNumbersTable(db))
+	_, err = insertPhone(db, "12345677890")
+	must(err)
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func createPhoneNumbersTable(db *sql.DB) error {
+	statement := fmt.Sprintf(`
+	  CREATE TABLE IF NOT EXISTS phone_numbers(
+		id SERIAL,
+		value VARCHAR(255)
+	  )`)
+	_, err := db.Exec(statement)
+	return err
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDb(db, name)
+}
+
+func createDb(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	return err
+}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("\\D")
